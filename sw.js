b67dbf6d8dfa1f7c6a52bfcab3058fc42fd103a3
kyone-kyona <samchew98@gmail.com>
2026-05-31 10:47:35 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,58 @@
-self.addEventListener('install', function() { self.skipWaiting(); });
+const CACHE = 'watermark-v1';
+const FILES = [
+  '/watermark-stamp/',
+  '/watermark-stamp/index.html',
+  '/watermark-stamp/manifest.json',
+  '/watermark-stamp/sw.js',
+  '/watermark-stamp/icon.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(FILES);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k){ return k !== CACHE; })
+            .map(function(k){ return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  var url = e.request.url;
+  if (url.indexOf('nominatim.openstreetmap.org') > -1) {
+    e.respondWith(
+      fetch(e.request).catch(function() {
+        return new Response(JSON.stringify({address:{}}), {
+          headers: {'Content-Type': 'application/json'}
+        });
+      })
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).then(function(response) {
+        return caches.open(CACHE).then(function(cache) {
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    }).catch(function() {
+      return caches.match('/watermark-stamp/index.html');
+    })
+  );
+});self.addEventListener('install', function() { self.skipWaiting(); });
 self.addEventListener('activate', function(e) {
   e.waitUntil(caches.keys().then(function(keys) {
     return Promise.all(keys.map(function(k){ return caches.delete(k); }));
